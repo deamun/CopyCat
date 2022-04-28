21a9f2e5162df9d18159da52ca713d93271dfbc1
Dean <dean_mundey@hotmail.com>
2022-04-28 15:28:33 +0100

    Step 6 - Include styles in render method

--- src/components/CopyCat.js
+++ src/components/CopyCat.js
@@ -44,6 +44,7 @@
 // ReactDOM.render(<CopyCat />, document.getElementById('app'));
 
 import React from 'react';
+import {styles} from '../styles';
 
 const images = {
   copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
@@ -57,12 +58,13 @@ class CopyCat extends React.Component {
     const toggleTape = this.props.toggleTape
     
     return (
-      <div>
-        <h1>Copy Cat</h1>
+      <div style={styles.divStyles}>
+        <h1 style={{marginBottom: '80px'}}>Copy Cat</h1>
         <img 
           alt='cat'
           src={copying ? images.copycat : images.quietcat}
           onClick={toggleTape}
+          style={styles.imgStyles}
         />
       </div>
     );
